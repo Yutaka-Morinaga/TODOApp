99a040afd4ce29644efe37627b74d4cd18591b6b
Yutaka-Morinaga <yutaka_morinaga@mail.toyota.co.jp>
2022-03-20 19:03:01 +0900

    ローカルストレージにリストを保存、リロードしても値が消えない

--- TODOapp.js
+++ TODOapp.js
@@ -2,6 +2,14 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const ul = document.getElementById("ul");
 
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todos.forEach(todo => {
+    add(todo);
+  })
+}
+
 // 入力内容を取得
 form.addEventListener("submit", function (event){
     event.preventDefault();
@@ -10,13 +18,27 @@ form.addEventListener("submit", function (event){
 });
 
 // 入力内容のリストを追加
-function add(){
+function add(todo){
   let todoText = input.value;
-  if (todoText.length > 0) {
+
+  if (todo) {
+    todoText = todo;
+  }
+  if (todoText.length > 0) { //空文字ではリストが作成されない
      const li = document.createElement("li");
     li.innerText = todoText;
     li.classList.add("list-group-item");
     ul.appendChild(li);
     input.value = "";
+    saveDate();
   }
+}
+
+function saveDate(){
+  const lists = document.querySelectorAll("li");
+  let todos = [];
+  lists.forEach(list => {
+    todos.push(list.innerText);
+  });
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
